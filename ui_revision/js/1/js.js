edf9daccc7fc55600334f93589f0f9ac82441753
prashantsingamsetti1705 <prashantsingamsetti1705@gmail.com>
2025-04-14 18:41:39 +0530

    js basics data types var etc...

---
+++ ui_revision/js/1/js.js
@@ -0,0 +1,27 @@
+// type conversion on js
+// it wiil conert into one data type to another data type
+// let a=12
+// console.log(typeof String(a));
+// // anothre method to convert into string
+// let b=12;
+// b=b.toString()
+// console.log(b)
+// console.log(typeof b);
+// string method in JS
+// index in [0]
+// let s="prashant ";
+// console.log(s[3])
+// console.log(s.indexOf("a"))
+// console.log(s.lastIndexOf("a"));
+// console.log(s.endsWith("t"));// it give you a bolean value
+// console.log(s.includes("y"));// it give you a boolean value
+// console.log(s.slice(0,4));
+// console.log(s.split(" "))// it will split into array
+// console.log(s.replace("p","P"))// it will rplace the value
+// console.log(s.length)
+// // lower case // upper case
+// console.log(s.substring(0,4));
+// console.log(s.substr(3,4))
+s="prashant"
+let heading=document.getElementById("myheading")
+heading.innerText=s;
---
+++ ui_revision/js/srcipt.js
@@ -0,0 +1,42 @@
+// alert("hello wrold")
+
+// // 
+// console.log("hello world");
+// // it same like problem
+// //  var in js
+
+// // let x=10;
+// // let ram=10*x;
+// // console.log(ram);
+// let x=10;
+// console.log(x);
+// x=20;
+// console.log(x);
+// const y=20
+// // it used for contstant value
+// console.log(y)
+// // diff in let and var
+
+// let a=10;
+// console.log(typeof a);
+// // data type
+// // premitive data type 
+// // 1number boolean string null undefined 
+// let b=10.5
+// let c="hello"
+// let d=true
+// let e
+// console.log(e)
+// let f=null
+// console.log(f)
+// console.log(typeof b,c,d,e,f)
+// 
+let x1=10;
+let y1=true
+console.log(x1+12+y1)
+let x2="prashant"
+// let x2=`prashant`
+let y2="singamsetti"
+console.log("hi ra"+" "+x2+y2)
+//let m= console.log("hira" ${y2})
+// console.log("hra prashant ${m}")
