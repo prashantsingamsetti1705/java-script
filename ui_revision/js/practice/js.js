77152ef3fe270162d0a93ddcecf1f91aae5b1663
prashantsingamsetti1705 <prashantsingamsetti1705@gmail.com>
2025-04-18 21:26:50 +0530

    basic,str oprartion

---
+++ ui_revision/js/practice/js.js
@@ -0,0 +1,44 @@
+// basics of the js and how to run the js code we use the node ctrl+f5 to run
+// console.log("hello world");
+// // like python we use print() but in js we use console.log()
+// // let,var,const are the varibels are used in js
+// // so we can store the varbiles and values but in python by varble=value;
+// let a=10;
+// console.log(a);
+// // or we can var
+// var a1=100;
+// console.log(a1)
+// // or we can use const to srore it andd etcc
+// const a2=1000;
+// console.log(a2);
+// // byt we can store the constant value and we can not chane
+// a=200
+// console.log(a);
+// in js we have the data types like primitive data types like number ,string,boolean,null,notdefined,non primeteive are like array,object
+let a3=100;//number
+console.log(a3);
+// we can store like decmial values like 0.0,1.5...etc
+let a4=10.5;//number
+console.log(a4);
+// we can store the string the value like"hello world"
+let a5="hello world";// string
+console.log(a5);
+// we can store like boolen values like ture or false 
+let a6=true;// boolean or// flase 
+console.log(a6);
+// we can store the value like null or undefined
+let a=null // we get the null values
+console.log(a)
+let a7
+console.log(a7)// we get the undefined values
+// we can store the value lie array and object in python like list and dict
+let a8=[1,2,3,4,5]
+console.log(a8)// we get the array values
+// in object it like key and value pair like in python dict
+let a9={
+    name:"hello",
+    class:"world",
+    age:100,
+
+}
+console.log(a9)// we get the object values
---
+++ ui_revision/js/practice/string.js
@@ -0,0 +1,22 @@
+// now we are working with the string on js and functions on strings
+// the  comment in js is like // and /* */ we use this for comments
+// now string function
+let str="hello world"
+console.log(str)// we get the string values 
+// to find the lenght of the string in js we use the length 
+console.log(str.length)// so the length of the string is 11
+// to find the index of the string we s[0]== the value
+console.log(str[0])// h
+console.log(str[1]=12)
+console.log(str.lastIndexOf("l"))
+console.log(str.indexOf("l"));
+console.log(str.slice(0,5))
+console.log(str.toUpperCase())
+console.log(str.toLowerCase())
+let str1="  hello prshanth     "
+console.log(str1.trim())
+//we are using the split()
+// it will split the string in to the array of the string
+let str2="hello world in js"
+console.log(str2.split(" "))// ['hello', 'world', 'in', 'js']
+// we  read in doc to understand in documention
