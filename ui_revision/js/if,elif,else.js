4551f3f73a92f43c9f182a0058ea1356b77a2ed4
prashantsingamsetti1705 <prashantsingamsetti1705@gmail.com>
2025-04-18 23:10:23 +0530

    loop in js

---
+++ ui_revision/js/if,elif,else.js
@@ -0,0 +1,47 @@
+// write a js program given age is algibel for vote or not
+let age=22
+if (age>=18){
+console.log("he elgibel for vote")
+}
+else{
+    console.log("not elgibel for vote")
+}
+// write a js program given number is multiple of 5
+let a=23
+if (a%5===0){
+console.log("it is divisble by 5")
+}
+else{
+    console.log("it not divisble by 5")
+}
+
+// Qs2. Write a code which can give grades to students according to their scores:
+// 80-100, A
+// 70-89, B
+// 60-69, C
+// 50-59, D
+// 0-49, F
+
+let grade=90
+if (grade>=80 && grade<=100){
+    console.log(`the prahant result is ${grade} and is grade is A`)
+}
+else if(grade>=70 && grade<=79){
+    console.log("b");
+} 
+else{
+console.log("c")
+}
+// switch case in js
+let color='black';
+switch (color){
+case "black":
+console.log("u correct ji");
+break
+case bllue:
+    console.log("u correct ji");
+break
+default :
+console.log("u not coorect ji")
+break
+}
---
+++ ui_revision/js/loops.js
@@ -0,0 +1,17 @@
+// the loop in js are for loop,while,do while
+// for(i=1,i<5,i++)
+// write a js program given number element are even or not
+let a=5
+sum=0
+for (i=1;i<=5;i++){ if ( i% 2==0){
+    sum=sum+1}
+}
+console.log(sum)
+/// off loop
+let a1="anill"
+l=0
+for (let i in a1){
+    console.log("i",i)
+l+=1
+}
+console.log(l)
