51a0f38281fed1ba497ef2210707a1ee8c91c2cc
prashantsingamsetti1705 <prashantsingamsetti1705@gmail.com>
2025-04-19 20:08:43 +0530

    array method

---
+++ ui_revision/js/practice/arraymethod.js
@@ -0,0 +1,28 @@
+// forEach(callbackfunction)
+let arr=['anil',"delhi"];
+arr.forEach((val)=>{
+    console.log(val.toUpperCase());
+})
+
+let arr2=[1,2,4];
+arr2.forEach((val)=>{
+    console.log(val**2)
+})
+ // or
+ let my_func=(arr2)=>{
+    console.log(arr2*arr2);
+ }
+ arr2.forEach(my_func) ;
+
+ // workinf onth emap
+//  let old_array=[65,17,22,45]
+//  let new_array=old_array.map((val)=>{
+//     return val*2
+//  })
+//  console.log(new_array)
+// filter
+let arr3=[1,2,3,4]
+let newarr=arr3.reduce((val,curr)=>{
+return val+curr;
+})
+console.log(newarr);
