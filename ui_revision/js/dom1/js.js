3c261f64da8b5eeea93fe4a615566b7c1b921f67
prashantsingamsetti1705 <prashantsingamsetti1705@gmail.com>
2025-04-21 19:36:16 +0530

    dom

---
+++ ui_revision/js/dom1/js.js
@@ -0,0 +1,41 @@
+//dom document object model 
+// 4 pillar of the dom 
+// 1.selection of an element --> it ment that elment reton html and css
+// 2.changing html
+// 3.changing css
+// 4.event listener
+
+
+// var a= document.querySelector("h1")// selecting on html elemnts
+// console.log(a);
+// chaning html
+//var a= document.querySelector("h1")
+// a.innerHTML="changed html"
+// console.log(a)
+// chaning css
+// let b=document.querySelector("h1")
+// b.style.color="red"// we are camilcas
+// b.style.backgroundColor="black"
+// console.log(b)
+// evenlistner are perfoming the operation & observer
+// var c=document.querySelector("h1")
+// c.innerHTML="kaise app log"
+// c.addEventListener("click",()=>{
+// c.innerHTML="mei badsha hu "
+// c.style.color="yellow"
+// c.style.backgroundColor="black"
+// })
+
+// simple example
+// let a =document.querySelector("#bulb")
+// let btn=document.querySelector("button")
+// let flag=0
+// btn.addEventListener("click",()=>{
+//     if (flag==0){
+//  a.style.backgroundColor="yellow"
+//  flag=1
+//     }else{
+//         a.style.backgroundColor="transparent"
+//         flag=0 
+//     }
+// })
