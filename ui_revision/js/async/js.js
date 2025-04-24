f49b5a1c0c20b6e4aa451dbc77b530fe7926b764
prashantsingamsetti1705 <prashantsingamsetti1705@gmail.com>
2025-04-24 14:02:49 +0530

    async

---
+++ ui_revision/js/async/js.js
@@ -0,0 +1,27 @@
+// now we are working on the asysc java srcipt
+// it works on multi tasking exaple like store doing payment like upi take 20 min other person bill will check out in another tab 
+// call back,promise ,asyncwait
+// function example(name='abhi'){
+//     return `wass iam iamabhi ${name}`
+// }
+// const res=example();
+// console.log(res)
+//example
+// 
+// function example2(name="abhi"){
+// setTimeout(
+//         ()=>{
+//             for(let i=0;i<500;i++){
+//                 console.log(`${name},${Math.random()}`)
+//             }
+//         }
+//     ,3000);
+// }
+// example2();
+// alert("lastline")
+// console.log("lastline")
+// now we are working the example 3
+setTimeout(()=>{
+    alert("anil")
+},3000)
+console.log("lastline")
